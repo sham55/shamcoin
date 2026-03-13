9a24ce50f6eeec787febee2b0cf7c8b598427fbc
Sham Torio <yagatsham55@gmail.com>
2026-03-13 07:39:47 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBqbpfr_eMYAR9VHUdkE_O4699QZDlJPro",
+  authDomain: "reutherfeld.firebaseapp.com",
+  projectId: "reutherfeld",
+  storageBucket: "reutherfeld.appspot.com",
+  messagingSenderId: "560144149047",
+  appId: "1:560144149047:web:85c0c9a673c05c30e76db2"
+};
